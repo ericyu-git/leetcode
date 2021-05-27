d55f50422fa1f6925837334e2ff41b5a82e03672
ericyu-git <yangintoronto@yahoo.com>
2021-05-27 14:48:04 -0400

    modified addTwoNumber

--- 2. add two number.java
+++ 2. add two number.java
@@ -1,47 +1,19 @@
-class Solution { 
-
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { 
-
-        ListNode n= new ListNode(0); 
-
-        ListNode i=l1,j=l2,sum=n; 
-
-        int plus=0; 
-
-        int value=0; 
-
-        while(i!=null || j!=null){ 
-
-            int x=(i!=null)? i.val : 0; 
-
-            int y=(j!=null)? j.val : 0; 
-
-            if(x+y>=10){ 
-
-                value=(x+y)%10+plus; 
-
-                plus=1; 
-
-            }else{ 
-
-                value=(x+y)%10+plus; 
-
-                plus=0; 
-
-            } 
-
-            sum.next=new ListNode(value); 
-
-            sum=sum.next; 
-
-            i=i.next; 
-
-            j=j.next; 
-
-        } 
-
-       return n.next;  
-
-      } 
-
-} 
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode();
+        ListNode res = ans;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0 ){
+            ListNode curr = new ListNode(0);
+            int sum = ((l1 == null)? 0:l1.val) + ((l2 == null)? 0:l2.val) + carry;
+            curr.val = sum % 10;
+            carry = sum / 10;
+            ans.next = curr;
+            ans = curr;
+            
+            l1 = (l1 == null)? l1: l1.next;
+            l2 = (l2 == null)? l2: l2.next;
+        }
+        return res.next;
+      }
+}
